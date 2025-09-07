30a99f03861dcc0a37b8047f5018ce87a70262ca
Yor-Jihons <ryo.malon814@gmail.com>
2025-09-07 21:51:15 +0900

    Modified.

--- src/types/ChatListItem.ts
+++ src/types/ChatListItem.ts
@@ -2,6 +2,6 @@ export default interface ChatListItem{
     id: number;
     chat_name: string;
     aiType?: string;
-    created_at?: string;
-    updated_at?: string;
+    created_at: string;
+    updated_at: string;
 }
