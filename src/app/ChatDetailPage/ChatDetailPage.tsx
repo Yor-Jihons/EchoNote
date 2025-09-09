7871007e4bb37db856784cd7d0926c9ba42891f6
Yor-Jihons <ryo.malon814@gmail.com>
2025-09-09 21:51:34 +0900

    Fetching the chat-data.

--- src/app/ChatDetailPage/ChatDetailPage.tsx
+++ src/app/ChatDetailPage/ChatDetailPage.tsx
@@ -1,28 +1,29 @@
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-
-// 仮のデータストア（本来はデータベースから取得します）
-const dummyUsers = [
-  { id: 1, name: 'Alice', email: 'alice@example.com' },
-  { id: 2, name: 'Bob', email: 'bob@example.com' },
-  { id: 3, name: 'Charlie', email: 'charlie@example.com' },
-  { id: 4, name: 'Danny', email: 'danny@example.com' },
-  { id: 5, name: 'Eagle', email: 'eagle@example.com' },
-  { id: 6, name: 'Fabre', email: 'fabre@example.com' },
-  { id: 7, name: 'Ham', email: 'ham@example.com' },
-];
+import ChatInfo from '../../types/ChatInfo';
+import ChatListItem from '../../types/ChatListItem';
+import MessageListItem from '../../types/MessageListItem';
+import SummaryListItem from '../../types/SummaryListItem';
 
 function ChatDetailPage() {
-  // useParamsフックを使ってURLのパラメータを取得
   const { chatId } = useParams<{ chatId: string }>();
-  const [user, setUser] = useState<{ id: number; name: string; email: string } | null>(null);
+  const [chatInfo, setChatInfo] = useState<ChatInfo>();
+
+  const fetchChatInfo = async () => {
+    const data = await window.interprocessCommunication.fetchChatInfo( Number( chatId ) );
+
+    const ret = data.value as ChatInfo;
+
+    const retChat: ChatListItem = ret.chat;
+    const retMessages: MessageListItem[] = ret.messages;
+    const retSummary: SummaryListItem = ret.summary;
+    setChatInfo( { chat: retChat, messages: retMessages, summary: retSummary } as ChatInfo );
+  };
 
   useEffect(() => {
-    // URLパラメータのIDからユーザーを検索
-    // ここでipcRenderer.invokeを使ってデータベースからユーザー情報を取得する
-    const foundUser = dummyUsers.find((u) => u.id === Number(chatId));
-    setUser(foundUser || null);
-  }, [chatId]);
+    fetchChatInfo();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [] );
 
   /*
     The data which I need:
@@ -57,17 +58,19 @@ function ChatDetailPage() {
 */
   //runSample( 30, "Hello?" ); // TODO: Modify here.
 
-  if (!user) {
-    return <div>ユーザーが見つかりません。</div>;
+  if ( !chatInfo ) {
+    return <div>チャットが見つかりません。</div>;
   }
 
   return (
     <div>
+      {/*
       <h2>チャット詳細ページ</h2>
       <p>ID: {user.id}</p>
       <p>名前: {user.name}</p>
       <p>メールアドレス: {user.email}</p>
       <Link to="/users">一覧に戻る</Link>
+      */}
     </div>
   );
 }
