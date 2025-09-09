292aaeeea556f0bdee6fe190bc27910b817fb517
Yor-Jihons <ryo.malon814@gmail.com>
2025-09-09 16:52:31 +0900

    Deleted the ToDo.

--- src/components/LeftSide/LeftSide.tsx
+++ src/components/LeftSide/LeftSide.tsx
@@ -56,8 +56,6 @@ const LeftSide = () => {
 
         navigate( "/chats/" + newItem.value.id );
 
-        // TODO: Implenent here.
-
         window.interprocessCommunication.showMessageBox( "登録完了しました。" );
     }
 
