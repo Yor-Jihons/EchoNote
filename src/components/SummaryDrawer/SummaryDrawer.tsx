aebfc91204e03ae5b9c6c1d09eaa34cd6f9ae537
Yor-Jihons <ryo.malon814@gmail.com>
2025-09-13 22:26:02 +0900

    Modified the Drawer.

--- src/components/SummaryDrawer/SummaryDrawer.tsx
+++ src/components/SummaryDrawer/SummaryDrawer.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Drawer from 'react-modern-drawer';
 import 'react-modern-drawer/dist/index.css';
 import SummaryListItem from "../../types/SummaryListItem";
+import DOMPurify from 'dompurify';
+import styles from "./summarydrawer.module.css";
 
 interface Props {
     summary: SummaryListItem;
     isSummaryDrawerOpen: boolean;
     onClose: () => void;
+    onInput: ( newText: string ) => void;
 }
 
 /*
@@ -18,11 +21,27 @@ interface Props {
         * updated_at
 */
 
-const SummaryDrawer = ( { isSummaryDrawerOpen, summary, onClose }: Props ) => {
+const SummaryDrawer = ( { isSummaryDrawerOpen, summary, onInput, onClose }: Props ) => {
+    const [isEditMode, setIsEditMode] = useState<boolean>( false );
+    const [summaryText, setSummaryText] = useState<string>( summary.summary_txt );
+
+    const editButton_click = () => {
+        setIsEditMode( !isEditMode );
+    }
+
+    const textarea_input = ( event:  React.FormEvent<HTMLTextAreaElement> ) => {
+        const v = event.currentTarget.value;
+        setSummaryText( v );
+        onInput( v );
+    }
+
     return (
         <React.Fragment>
-            <Drawer open={isSummaryDrawerOpen} onClose={onClose} direction="bottom">
-                <p>{summary?.summary_txt}</p>
+            <Drawer open={isSummaryDrawerOpen} onClose={onClose} direction="bottom" className={styles.drawer}>
+                <button className={styles.edit_button} onClick={editButton_click}>編集モードへ</button>
+                {!isEditMode ? <div><textarea className={styles.text_input} defaultValue={summaryText} onInput={textarea_input}></textarea></div>
+                    : <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize( summaryText ) }} />}
+                <button>OK</button>
             </Drawer>
         </React.Fragment>
     );
