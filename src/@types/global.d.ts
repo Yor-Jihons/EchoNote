e816ada6a30928effd6dfaa499240f97f5df535b
Yor-Jihons <ryo.malon814@gmail.com>
2026-01-20 17:29:30 +0900

    feat: IPCResultの追加

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,6 +1,7 @@
 import ChatListItem from "../types/ChatListItem";
 import MessageListItem from "../types/MessageListItem";
 import SummaryListItem from "../types/SummaryListItem";
+import IPCResult from "../types/IPCResult";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export interface IInterprocessCommunication {
@@ -8,11 +9,11 @@ export interface IInterprocessCommunication {
   getSystemLocale: () => Promise<string>;
   fetchChats: ( query: string ) => Promise<ChatListItem[]>;
   deleteChat: (id: number) => void;
-  addChat: ( chatName: string, aiType: string, description: string ) => Promise<{success: boolean, value: { chat: ChatListItem, summary: SummaryListItem }, errMessage?: string}>;
-  addMessage: ( chatId: number, orderInChat: number, senderId:number, messageText: string ) => Promise<{success: boolean, value: MessageListItem, errMessage?: string}>;
-  updateSummary: ( summaryId: number, newText: string ) => Promise<{success: boolean, value: SummaryListItem, errMessage?: string}>;
+  addChat: ( chatName: string, aiType: string, description: string ) => Promise<IPCResult<{ chat: ChatListItem, summary: SummaryListItem }>>;
+  addMessage: ( chatId: number, orderInChat: number, senderId:number, messageText: string ) => Promise<IPCResult<MessageListItem>>;
+  updateSummary: ( summaryId: number, newText: string ) => Promise<IPCResult<SummaryListItem>>;
   showMessageBox: ( message: string, buttons: string[] ) => Promise<number>; // Returns index of the button which the user selected.
-  updateMessage: ( messageId: number, newText: string ) => Promise<{success: boolean, value: MessageListItem, errMessage?: string}>;
+  updateMessage: ( messageId: number, newText: string ) => Promise<IPCResult<MessageListItem>>;
   sendMessageUpdated: () => void;
   onUpdateChatList: ( callback: () => void ) => void;
   removeUpdateChatListListener: ( callback: () => void ) => void;
